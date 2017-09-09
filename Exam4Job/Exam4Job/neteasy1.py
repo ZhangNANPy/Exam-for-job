d69897363b8b6a3168fef20873deb61fb3c3e5aa
ZhangNan <zhangrunnanshuai@163.com>
2017-09-09 16:53:44 +0800

    netEasy

---
+++ Exam4Job/Exam4Job/neteasy1.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+#魔法币
+
+n = 10
+
+def magicCoin(n):
+    re = []
+    if n == 1:
+        re.append(1)
+    elif n == 2:
+        re.append(2)
+    while n > 2:
+        if n % 2 == 0:
+            n = (n-2)/2
+            re.append(2)
+        else:
+            n = (n-1)/2
+            re.append(1)
+    re.append(int(n))
+    re.reverse()
+    return re
+
+if __name__ == "__main__":
+    print(''.join([str(i) for i in magicCoin(n)]))
