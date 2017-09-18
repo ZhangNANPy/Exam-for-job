da51df441253d4701f90d85169d9d2c549068897
ZhangNan <zhangrunnanshuai@163.com>
2017-09-18 21:51:55 +0800

    mi

---
+++ Exam4Job/Exam4Job/mix2.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+
+def YH(n):
+    if n == 1:
+        return 1
+    i = 1
+    while True:#控制行
+        up = 1
+        down = 1
+        i += 1
+        for j in range(1, int(i/2 + 1)):
+            up = up * (i - j + 1)
+            down = down * j
+            temp  = int(up / down)
+            if n == temp:
+                return i
+            elif n < temp:
+                break
+
+if __name__ == "__main__":
+    n = int(input())
+    print(YH(n))
