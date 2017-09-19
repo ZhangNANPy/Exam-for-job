650f832e7efbc17033b98786874463be06e51f98
ZhangNan <zhangrunnanshuai@163.com>
2017-09-19 15:52:03 +0800

    correct

--- Exam4Job/Exam4Job/mix2.py
+++ Exam4Job/Exam4Job/mix2.py
@@ -2,16 +2,16 @@
 
 def YH(n):
     if n == 1:
-        return 1
+        return 0
     i = 1
     while True:#控制行
         up = 1
         down = 1
         i += 1
-        for j in range(1, int(i/2 + 1)):
+        for j in range(1, int((i + 1) / 2 + 1)):
             up = up * (i - j + 1)
             down = down * j
-            temp  = int(up / down)
+            temp = int(up / down)
             if n == temp:
                 return i
             elif n < temp:
