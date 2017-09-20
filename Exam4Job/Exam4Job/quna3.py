7e98908f3ca5879acc5cd2116dc91008d5628305
ZhangNan <zhangrunnanshuai@163.com>
2017-09-20 11:11:44 +0800

    quna

---
+++ Exam4Job/Exam4Job/quna3.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+def myPow(a,b):
+    #b = bin(b);
+    #i = len(b) - 1
+    res = 1
+    base = a
+    while b != 0:
+        if b & 1 == 1:
+            res *= base
+        base *= base
+        b = b >> 1
+    return res
+
+if __name__ == '__main__':
+    (a, b, m) = input().split()
+    print(myPow(int(a), int(b)) % int(m))
---
+++ Exam4Job/Exam4Job/thunder1.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+import re
+def solve(eq, var = 'x'):
+    eq1 = eq.replace('=', "-(")+")"
+    c = eval(eq1, {var: 1j})
+    return -c.real / c.imag
+
+def pre(s):
+    l = list(s)
+    i = 1
+    while i < len(l):
+        if (l[i-1].isdigit() and l[i] == 'x') or (l[i-1].isdigit() and l[i] == '('):
+            l.insert(i, '*')
+            i+=1
+        i += 1
+    return "".join(l)
+
+if __name__ == '__main__':
+    s = input()
+    print(solve(pre(s)))#2x+5-3+x=6+x-2
